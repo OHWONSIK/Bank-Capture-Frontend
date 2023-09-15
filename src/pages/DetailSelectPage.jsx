5176c8808ed1bbe593b935208204ba8c731f4f17
WONSIK <omsoms777@naver.com>
2023-09-16 02:49:54 +0900

    #3 행원필터링
    시간 매핑해서 예약 불가능한 시간대 클릭안되게 구현해야함

--- src/pages/DetailSelectPage.jsx
+++ src/pages/DetailSelectPage.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { styled } from 'styled-components';
+import React, { useEffect, useState } from "react";
+import { styled } from "styled-components";
 
-import ReserveDate from '../components/ReserveDate';
-import ReserveTime from '../components/ReserveTime';
-import { useLocation, useNavigate } from 'react-router-dom';
+import ReserveDate from "../components/ReserveDate";
+import ReserveTime from "../components/ReserveTime";
+import { useLocation, useNavigate } from "react-router-dom";
 import { AiFillStar } from "react-icons/ai";
 import { BiSolidComment } from "react-icons/bi";
-import moment from 'moment';
+import moment from "moment";
 import Swal from "sweetalert2";
-// import { axios } from 'axios';
-
 
 function DetailSelectPage(props) {
-    const [selectedDate, setSelectedDate] = useState(new Date()); // 선택한 날짜
+    const [selectedDate, setSelectedDate] = useState(""); // 선택한 날짜
     const [selectedTime, setSelectedTime] = useState(""); // 선택한 시간
     const [isReserveActive, setIsReserveActive] = useState(false); // 예약이 활성화되었는지 여부
 
     const location = useLocation();
-    const selectedBankers = location.state.selectedBankers || [];
+
+    const taskId = location.state.taskId; //예약하기를 위해 필요한 업무ID
+    const selectedBankers = location.state.selectedBankers || []; //지점, 업무에 맞는 모든 행원들
 
     // 시간&&날짜 만족하는 은행원 박스만 활성화시키는 코드 작성해야함
     // const [selectedDate, setSelectedDate] = useState(new Date());
@@ -32,7 +32,7 @@ function DetailSelectPage(props) {
     //         params: {date, time,},
     //     })
     //     .then(res => {
-            
+
     //     })
     // }
 
@@ -41,93 +41,154 @@ function DetailSelectPage(props) {
         setSelectedDate(date);
         setSelectedTime(""); // 날짜가 변경되면 시간 선택 초기화
         setIsReserveActive(false); // 날짜 선택 시 예약 비활성화
+    };
 
-    }
+    // 시간 선택 후 확인 버튼을 클릭할 때 실행되는 함수
+    const handleTimeSelection = (time) => {
+        setSelectedTime(time);
+        setIsReserveActive(true); // 시간 선택 시 예약 활성화
+    };
 
-   // 시간 선택 후 확인 버튼을 클릭할 때 실행되는 함수
-   const handleTimeSelection = (time) => {
-    setSelectedTime(time);
-    setIsReserveActive(true); // 시간 선택 시 예약 활성화
-}
+    //필터링된 은행원 목록
+    //아직 시간 매핑 구현안해서 10시에 가능한 사람들로만 테스트함
+    const filteredBankers = isReserveActive
+        ? selectedBankers.filter((banker) => {
+              const hasSchedule = banker.scheduleList.some((schedule) => {
+                  return (
+                      schedule.scheduleDate ===
+                          moment(selectedDate).format("YYYYMMDD") &&
+                      (String(schedule.time1 + 9) + ":00").includes(
+                          selectedTime
+                      )
+                  );
+              });
+              return hasSchedule;
+          })
+        : selectedBankers.filter((banker) => {
+              const hasSchedule =
+                  selectedDate === ""
+                      ? banker.scheduleList
+                      : banker.scheduleList.some((schedule) => {
+                            return (
+                                schedule.scheduleDate ===
+                                moment(selectedDate).format("YYYYMMDD")
+                            );
+                        });
 
-  
-//필터링된 은행원 목록
-const filteredBankers = isReserveActive
-? selectedBankers.filter(banker => {
-    const hasSchedule = banker.schedule_list.some(schedule => {
-        return (
-            schedule.date === moment(selectedDate).format("YYYY-MM-DD") &&
-            schedule.time.includes(selectedTime)
-        );
-    });
-    return hasSchedule;
-})
-: selectedBankers.filter(banker=> {
-    const hasSchedule = banker.schedule_list.some(schedule => {
-        return (
-            schedule.date === moment(selectedDate).format("YYYY-MM-DD"));});
-            return hasSchedule;})
-
+              return hasSchedule;
+          });
 
     const navigate = useNavigate();
 
     const moveToBankerSelect = (selectedBankerId) => {
-        if(!isReserveActive)
-            {Swal.fire(
-                '시간이 선택되지 않았습니다',
-                '원하는 시간을 선택해주세요',
-                'error'
-              )
-                return;}
-        const selectedBanker = selectedBankers.find(banker =>
-            banker.id === selectedBankerId);
-        if (selectedBanker) {
-            navigate('/banker-select', {state: {selectedBanker}});
+        if (!isReserveActive) {
+            Swal.fire(
+                "시간이 선택되지 않았습니다",
+                "원하는 시간을 선택해주세요",
+                "error"
+            );
+            return;
         }
-    }
-    console.log("filteredBankers:", filteredBankers);
+        const selectedBanker = selectedBankers.find(
+            (banker) => banker.bankerId === selectedBankerId
+        );
+        if (selectedBanker) {
+            
+            //캘린더 라이브러리 클릭시 선택되는 날짜포맷을 YYYYMMDD형식으로 변경해서 저장
+            let reservationDate = moment(selectedDate).format("YYYYMMDD");
+            
+            //예약시간 time번호로 매핑
+            let reservationTime =
+                selectedTime === "10:00"
+                    ? 1
+                    : selectedTime === "11:00"
+                    ? 2
+                    : selectedTime === "13:00"
+                    ? 3
+                    : selectedTime === "14:00"
+                    ? 4
+                    : selectedTime === "15:00"
+                    ? 5
+                    : selectedTime === "16:00"
+                    ? 6
+                    : selectedTime === "17:00"
+                    ? 7 : "";
+            
+            //행원의 스케줄리스트에 bankId가 있기때문에 가져와서 저장
+            //나중에 지점선택할때부터 bankId물고오는걸로 수정해야함
+;           const bankId = selectedBanker.scheduleList[0].bankId;
+            
+            navigate("/banker-select", {
+                state: {
+                    selectedBanker,
+                    reservationDate,
+                    reservationTime,
+                    bankId: bankId,
+                    taskId,
+                },
+            });
+        }
+    };
+    //console.log("filteredBankers:", filteredBankers);
 
     // 예제 2: ReserveDate 컴포넌트에서 선택한 날짜를 콘솔에 출력
-    console.log("selectedDate:", selectedDate);
-    
-    console.log("selectedBankerList",selectedBankers);
+    //console.log("selectedDate:", selectedDate);
+
+    //console.log("selectedBankerList", selectedBankers);
     // 예제 3: ReserveTime 컴포넌트에서 선택한 시간을 콘솔에 출력
-    console.log("selectedTime:", selectedTime);
+    //console.log("selectedTime:", selectedTime);
     return (
         <Container>
             <SubContainer>
                 <LeftContainer>
                     <DateSelect>날짜 선택</DateSelect>
-                    <ReserveDate selectedDate={selectedDate} setSelectedDate={handleDateChange} />
+                    <ReserveDate
+                        selectedDate={selectedDate}
+                        setSelectedDate={handleDateChange}
+                    />
 
                     <TimeSelect>시간 선택</TimeSelect>
-                    <ReserveTime selectedTime={selectedTime} setSelectedTime={handleTimeSelection} />
-                
-             
-              
-               
+                    <ReserveTime
+                        selectedTime={selectedTime}
+                        setSelectedTime={handleTimeSelection}
+                    />
                 </LeftContainer>
 
                 <RightContainer>
                     {filteredBankers.map((banker, i) => (
-                        <BankerInfo key={i} onClick={() =>moveToBankerSelect(banker.id)}>
-                            <Profile src={banker.banker_imgepath} alt={"프로필 이미지"}></Profile>
+                        <BankerInfo
+                            key={i}
+                            onClick={() => moveToBankerSelect(banker.bankerId)}
+                        >
+                            <Profile
+                                src={banker.bankerImgPath}
+                                alt={"프로필 이미지"}
+                            ></Profile>
                             <Text>
-                                <Name>행원 {banker.banker_name}</Name>
+                                <Name>행원 {banker.bankerName}</Name>
                                 <PrAndCareer>
-                                    <Pr>{banker.banker_info}</Pr>
-                                    <Career>({banker.banker_career})</Career>
+                                    <Pr>{banker.bankerInfo}</Pr>
+                                    <Career>({banker.bankerCareer})</Career>
                                 </PrAndCareer>
                                 <RatingAndComment>
-                                    <Rating><AiFillStar style={{marginRight : "5px"}}/>{banker.banker_avg_star.toFixed(1)}</Rating>
-                                    <Comment><BiSolidComment style={{marginRight : "5px"}}/>{banker.banker_cnt_comment}</Comment>
+                                    <Rating>
+                                        <AiFillStar
+                                            style={{ marginRight: "5px" }}
+                                        />
+                                        {banker.bankerAvgStar}
+                                    </Rating>
+                                    <Comment>
+                                        <BiSolidComment
+                                            style={{ marginRight: "5px" }}
+                                        />
+                                        {banker.bankerCommentCnt}
+                                    </Comment>
                                 </RatingAndComment>
                             </Text>
                         </BankerInfo>
                     ))}
                 </RightContainer>
             </SubContainer>
-          
         </Container>
     );
 }
@@ -146,7 +207,6 @@ const SubContainer = styled.div`
     display: flex;
     padding-top: 50px;
     height: 800px;
-    
 `;
 
 const LeftContainer = styled.div`
@@ -161,9 +221,9 @@ const RightContainer = styled.div`
     flex: 1;
     /* border: 1px solid black; */
     overflow-y: scroll;
-  &::-webkit-scrollbar {
-    display: none;
-  }
+    &::-webkit-scrollbar {
+        display: none;
+    }
 `;
 
 const DateSelect = styled.div`
@@ -184,10 +244,7 @@ const TimeSelect = styled.div`
     margin-bottom: 20px;
     border-bottom: 2px solid lightgray;
     padding-left: 20px;
-
 `;
-
-
 
 const SelectBtn = styled.button`
     width: 200px;
@@ -208,7 +265,6 @@ const BankerInfo = styled.div`
     padding: 40px;
     border-bottom: 1px solid lightgray;
     cursor: pointer;
-
 `;
 
 const Text = styled.div`
@@ -219,9 +275,9 @@ const Text = styled.div`
 `;
 
 const Profile = styled.img`
-width: 100px;
-height: 100px;
-border-radius: 50px;
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
 `;
 
 const Name = styled.div`
@@ -230,13 +286,12 @@ const Name = styled.div`
 `;
 
 const PrAndCareer = styled.div`
- display: flex;
+    display: flex;
     align-items: center;
 `;
 
 const Pr = styled.div`
     font-size: 18px;
-    
 `;
 
 const Career = styled.div`
@@ -245,23 +300,21 @@ const Career = styled.div`
 `;
 
 const RatingAndComment = styled.div`
-width: 100px;
+    width: 100px;
     display: flex;
     align-items: center;
-justify-content: space-between;
+    justify-content: space-between;
     font-size: 18px;
     /* margin-top: 5px; */
 `;
 
 const Rating = styled.div`
-
     display: flex;
     align-items: center;
     justify-content: space-between;
 `;
 
 const Comment = styled.div`
-
     display: flex;
     align-items: center;
     justify-content: space-between;
