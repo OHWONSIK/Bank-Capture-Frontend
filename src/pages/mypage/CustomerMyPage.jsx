09d7f9f18bd553e2d7dd3840e1268f735667e938
WONSIK <omsoms777@naver.com>
2023-09-17 14:36:02 +0900

    #5 날짜형식 수정

--- src/pages/mypage/CustomerMyPage.jsx
+++ src/pages/mypage/CustomerMyPage.jsx
@@ -64,7 +64,7 @@ function CustomerMyPage(props) {
             const year = dateStr.substring(0, 4);
             const month = dateStr.substring(4, 6);
             const day = dateStr.substring(6, 8);
-            return `${year}-${month}-${day}`;
+            return `${year}.${month}.${day}`;
         }
         return dateStr; // 이미 변환된 날짜는 그대로 반환
     };
