aea25c9bee495316b6fc6ff28e5f966acd72d211
oU-Ua <imuu30071126@gmail.com>
2023-09-18 23:36:41 +0900

    #12 checkTimePage 오류 수

--- src/pages/mypage/CheckTimePage.jsx
+++ src/pages/mypage/CheckTimePage.jsx
@@ -4,8 +4,7 @@ import { styled } from "styled-components";
 import ScheduleDate from "../../components/ScheduleDate";
 import ScheduleTime from "../../components/ScheduleTime";
 import { useLocation, useNavigate } from "react-router-dom";
-import { AiFillStar } from "react-icons/ai";
-import { BiSolidComment } from "react-icons/bi";
+import { FiCalendar } from "react-icons/fi";
 import moment from "moment";
 import Swal from "sweetalert2";
 import { API } from "../../config";
@@ -14,16 +13,13 @@ import axios from "axios";
 
 function CheckTimePage(props) {
     const [selectedDate, setSelectedDate] = useState(""); // 선택한 날짜
-    const [selectedTime, setSelectedTime] = useState(""); // 선택한 시간
+    const [selectedTime, setSelectedTime] = useState([]); // 선택한 시간
     const [isReserveActive, setIsReserveActive] = useState(false); // 예약이 활성화되었는지 여부
     
-    const location = useLocation();
+    const bankerId = sessionStorage.getItem('bankerId');
 
-    const bankerId = sessionStorage.getItem('bankerId')
-
-    const selectedBankers = location.state.selectedBankers || []; //지점, 업무에 맞는 모든 행원들
     
-    const [filteredBankers, setFilteredBankers] = useState(selectedBankers);
+
 
     //시간매핑 배열
     const times = ["10:00", "11:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
@@ -37,112 +33,71 @@ function CheckTimePage(props) {
     const handleTimeSelection = (time) => {
         setSelectedTime(time);
         setIsReserveActive(true); // 시간 선택 시 예약 활성화
-
-        const filtered = selectedBankers.filter((banker) => {
-            const scheduleForDate = banker.scheduleList.find((schedule) => {
-                return (
-                    schedule.scheduleDate ===
-                    moment(selectedDate).format("YYYYMMDD")
-                );
-            });
-
-            if (scheduleForDate) {
-                // 선택한 시간에 대한 예약 가능 여부 확인
-                const selectedTimeKey = `time${getTimeSlot(time)}`;
-                return scheduleForDate[selectedTimeKey] === 1;
-            }
-
-            return false; 
-        });
-
-        setFilteredBankers(filtered);
-
-        // 예약 가능한 행원이 없을 때 알림창 띄우기
-        if (filtered.length === 0) {
-            Swal.fire(
-                "예약 가능한 행원이 없습니다.",
-                "다른 시간을 선택하세요.",
-                "error"
-            );
-        }
     };
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
-        setSelectedTime(""); // 날짜가 변경되면 시간 선택 초기화
+        setSelectedTime([]); // 날짜가 변경되면 시간 선택 초기화
         setIsReserveActive(false); // 날짜 선택 시 예약 비활성화
 
-        const filtered = selectedBankers.filter((banker) => {
-            const scheduleForDate = banker.scheduleList.find((schedule) => {
-                return (
-                    schedule.scheduleDate === moment(date).format("YYYYMMDD")
-                );
-            });
-
-            if (scheduleForDate) {
-                // 선택한 날짜에 예약이 가능한 행원들 표시
-                return scheduleForDate;
-            }
-
-            return false;
-        });
-
-        setFilteredBankers(filtered);
-
-        // 예약 가능한 행원이 없을 때 알림창 띄우기
-        if (filtered.length === 0) {
-            Swal.fire(
-                "예약 가능한 행원이 없습니다.",
-                "다른 날짜를 선택하세요.",
-                "error"
-            );
-        }
+        
     };
 
     const navigate = useNavigate();
 
-    const moveToBankerSelect = (selectedBankerId) => {
-        if (!isReserveActive) {
-            Swal.fire(
-                "시간이 선택되지 않았습니다",
-                "원하는 시간을 선택해주세요",
-                "error"
-            );
-            return;
-        }
-        const selectedBanker = selectedBankers.find(
-            (banker) => banker.bankerId === selectedBankerId
-        );
-        if (selectedBanker) {
-            
-            //캘린더 라이브러리 클릭시 선택되는 날짜포맷을 YYYYMMDD형식으로 변경해서 저장
-            let reservationDate = moment(selectedDate).format("YYYYMMDD");
-            let reservationTime = getTimeSlot(selectedTime);
-            
-            //행원의 스케줄리스트에 bankId가 있기때문에 가져와서 저장
-            //나중에 지점선택할때부터 bankId물고오는걸로 수정해야함
-            const bankId = selectedBanker.scheduleList[0].bankId;
-            
-            navigate("/banker-select", {
-                state: {
-                    reservationId,
-                    selectedBanker,
-                    reservationDate,
-                    reservationTime,
-                    bankId: bankId,
-                    taskId,
-                },
-            });
-        }
-    };
-    //console.log("filteredBankers:", filteredBankers);
+    
 
-    // 예제 2: ReserveDate 컴포넌트에서 선택한 날짜를 콘솔에 출력
-    //console.log("selectedDate:", selectedDate);
+    function saveSchedule() {
+        Swal.fire({
+            title: "스케줄을 저장하시겠습니까?",
+            text: "선택한 시간은 예약가능한 시간으로 표시됩니다.",
+            icon: "info",
+            showCancelButton: true,
+            confirmButtonColor: "black",
+            cancelButtonColor: "#696969",
+            cancelButtonText: "뒤로가기",
+            confirmButtonText: "저장하기",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                console.log(selectedTime);
+                const selectionStatusArray = times.map((time) =>
+                selectedTime.includes(time) ? 1 : 0);
 
-    //console.log("selectedBankerList", selectedBankers);
-    // 예제 3: ReserveTime 컴포넌트에서 선택한 시간을 콘솔에 출력
-    //console.log("selectedTime:", selectedTime);
+            console.log(selectionStatusArray);
+                let scheduleDate = moment(selectedDate).format("YYYYMMDD");
+                console.log(scheduleDate);
+                axios
+                    .post(`${API.SCHEDULE_INSERT}`, {
+                        bankerId: bankerId,
+                        bankId:1,
+                        scheduleDate: scheduleDate,
+                        time1:selectionStatusArray[0],
+                        time2:selectionStatusArray[1],
+                        time3:selectionStatusArray[2],
+                        time4:selectionStatusArray[3],
+                        time5:selectionStatusArray[4],
+                        time6:selectionStatusArray[5],
+                        time7:selectionStatusArray[6],
+                    })
+                    
+                    .then((response) => {
+                        console.log("스케줄 저장 성공:", response.data);
+
+                        Swal.fire({
+                            title: "저장완료",
+                            text: "스케줄이 저장되었습니다.",
+                            icon: "success",
+                            confirmButtonColor: "#3085d6",
+                            confirmButtonText: "확인",
+                        });
+                    })
+                    .catch((error) => {
+                        console.error("스케줄 저장 에러:", error);
+                    });
+            }
+        });
+    }
+
     return (
         <Container>
             <SubContainer>
@@ -158,94 +113,18 @@ function CheckTimePage(props) {
                         selectedTime={selectedTime}
                         setSelectedTime={handleTimeSelection}
                     />
+                    <ReviewBtn 
+                        onClick={() =>
+                            saveSchedule()}>
+                                <FiCalendar
+                                style={{
+                                    marginRight: "10px",
+                                    color: "#00c154",
+                                }}/>
+                                 스케줄 저장
+                    </ReviewBtn>
+
                 </LeftContainer>
-
-                <RightContainer>
-                    {filteredBankers.map((banker, i) => (
-                        <BankerInfo
-                            key={i}
-                            onClick={() => moveToBankerSelect(banker.bankerId)}
-                        >
-                            <Profile
-                                src={banker.bankerImgPath}
-                                alt={"프로필 이미지"}
-                            ></Profile>
-                            <Text>
-                                <Name>행원 {banker.bankerName}</Name>
-                                <PrAndCareer>
-                                    <Pr>{banker.bankerInfo}</Pr>
-                                    <Career>({banker.bankerCareer})</Career>
-                                </PrAndCareer>
-                                <RatingAndComment>
-
-                                    {/* 별점 없을시 0.00 코멘트 없을시 0출력*/}
-                                    {/* <Rating>
-                                        <AiFillStar
-                                            style={{ marginRight: "5px" }}
-                                        />
-                                        {banker.bankerAvgStar.toFixed(2)}
-                                    </Rating>
-                                    
-                                    <Comment>
-                                        <BiSolidComment
-                                            style={{ marginRight: "5px" }}
-                                        />
-                                        {banker.bankerCommentCnt === null
-                                            ? "0"
-                                            : banker.bankerCommentCnt}
-                                    </Comment> */}
-                                    
-                                    {/*별점 코멘트 없을 시 아이콘만 표시*/}
-                                    <RatingAndComment>
-                                        {banker.bankerAvgStar > 0 && (
-                                            <Rating>
-                                                <AiFillStar
-                                                    style={{
-                                                        marginRight: "5px",
-                                                    }}
-                                                />
-                                                {banker.bankerAvgStar.toFixed(
-                                                    2
-                                                )}
-                                            </Rating>
-                                        )}
-
-                                        {banker.bankerAvgStar === 0 && (
-                                            <Rating>
-                                                <AiFillStar
-                                                    style={{
-                                                        marginRight: "5px",
-                                                    }}
-                                                />
-                                            </Rating>
-                                        )}
-
-                                        {banker.bankerCommentCnt !== null && (
-                                            <Comment>
-                                                <BiSolidComment
-                                                    style={{
-                                                        marginRight: "5px",
-                                                    }}
-                                                />
-                                                {banker.bankerCommentCnt}
-                                            </Comment>
-                                        )}
-
-                                        {banker.bankerCommentCnt === null && (
-                                            <Comment>
-                                                <BiSolidComment
-                                                    style={{
-                                                        marginRight: "5px",
-                                                    }}
-                                                />
-                                            </Comment>
-                                        )}
-                                    </RatingAndComment>
-                                </RatingAndComment>
-                            </Text>
-                        </BankerInfo>
-                    ))}
-                </RightContainer>
             </SubContainer>
         </Container>
     );
@@ -376,5 +255,20 @@ const Comment = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+`;
+const ReviewBtn = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #b0b0b0;
+    border-radius: 10px;
+    margin-top :15px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    font-size: 20px;
+    font-weight: 700;
+    cursor: pointer;
 `;
 export default CheckTimePage;
